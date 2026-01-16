ef610c30b5741ff87870bce1277b4fe45a73af47
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-01-16 11:03:43 +0100

    Editado archivo __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# Archivo de manifiesto para el módulo de gestión de tareas en Odoo
+{
+ 'name': 'Gestión de Tareas',
+ 'version': '1.0',
+ 'summary': 'Módulo para gestionar tareas individuales de los empleados',
+ 'category': 'Productivity',
+ 'author': 'Paco Navas',
+ 'website': 'https://tuweb.com',
+ 'license': 'LGPL-3',
+ 'depends': ['base', 'mail'],
+ 'icon': '/gestion_tareas/static/description/icon52.png',
+ 'data': [
+ 'views/gestion_tarea_views.xml',
+ 'security/ir.model.access.csv',
+ ],
+ 'application': True,
+ 'installable': True,
+ 'auto_install': False,
+ 'description': """
+ Módulo de Odoo para la gestión de tareas asignadas a empleados,
+ incluyendo vistas Kanban y formulario detallado.
+ """
+}
